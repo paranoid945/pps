5ebfbdf1f27953d5b3b6ed0ea11f99ed72e4bfe8
Mingyu <paranoid945@gmail.com>
2020-07-02 01:40:44 +0800

    123

--- shells/vps_network_bench.sh
+++ shells/vps_network_bench.sh
@@ -175,9 +175,9 @@ print_speedtest() {
 	printf "%-18s%-18s%-20s%-12s\n" " Node Name" "Upload Speed" "Download Speed" "Latency" | tee -a $log
 	speed_test '5145' 'Beijing Unicom'
 	speed_test '27154' 'TianJin 5G   CU'
-	speed_test '27594' 'Guangzhou 5G CT'
 	speed_test '24447' 'Shanghai 5G  CU'
 	speed_test '26678' 'Guangzhou 5G CU'
+	speed_test '27594' 'Guangzhou 5G CT'
     speed_test '27377' 'Beijing 5G   CT'
     speed_test '26352' 'Nanjing 5G   CT'
     speed_test '17145' 'Hefei 5G     CT'
