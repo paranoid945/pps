e3f319591dbd306087068dbb8bee5f28b6a338b2
Mingyu <paranoid945@gmail.com>
2020-07-13 22:50:11 +0800

    zzz

--- shells/centos8/install_bbr.sh
+++ shells/centos8/install_bbr.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
- wget --no-check-certificate https://github.com/sprov065/blog/raw/master/bbr.sh && bash bbr.sh
+sysctl net.ipv4.tcp_available_congestion_control
+wget --no-check-certificate https://github.com/sprov065/blog/raw/master/bbr.sh && bash bbr.sh
