bfb513475de2725c10bbd751fc5a44f1875c339e
Mingyu Jia <paranoid945@gmail.com>
2020-09-16 06:39:04 +0800

    er

--- shells/k8s/install_s4.sh
+++ shells/k8s/install_s4.sh
@@ -7,3 +7,5 @@ mkdir -p /etc/haproxy/
 cp check_apiserver.sh /etc/keepalived/
 cp keepalived_master.conf /etc/keepalived/keepalived.conf
 cp haproxy.cfg /etc/haproxy/
+cp keepalived.yaml /etc/kubernetes/manifests/
+cp haproxy.yaml /etc/kubernetes/manifests/
