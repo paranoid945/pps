8faec020e6c914a17b2486e938d7284f7cb7b855
Mingyu Jia <paranoid945@gmail.com>
2020-09-19 06:22:01 +0800

    123

--- shells/keepalived_haproxy/install_backup.sh
+++ shells/keepalived_haproxy/install_backup.sh
@@ -1,3 +1,5 @@
 yum install -y keepalived haproxy
 cp -rf haproxy.cfg /etc/haproxy/haproxy.cfg
 cp -rf keepalived_backup.conf /etc/keepalived/keepalived.conf
+systemctl restart haproxy
+systemctl restart keepalived
